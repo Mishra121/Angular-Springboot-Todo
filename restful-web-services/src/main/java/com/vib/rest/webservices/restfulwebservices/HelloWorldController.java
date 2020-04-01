e9f27e38fc6eeea04ae96713c71c52d636aa8124
Vibhu <vibhu98mishra@gmail.com>
2020-04-01 23:01:22 +0530

    java api learn work

--- restful-web-services/src/main/java/com/vib/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/vib/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,9 +1,12 @@
 package com.vib.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 	
@@ -15,5 +18,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPath(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
